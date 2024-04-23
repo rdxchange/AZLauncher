c7afbcf6ee8850a044294ff88c73c402cda5c0de
rdxchange <136237752+rdxchange@users.noreply.github.com>
2024-04-23 15:08:16 +0200

    Update PopupManager.java

--- src/main/java/fr/speccy/azclientapi/bukkit/fork/popup/PopupManager.java
+++ src/main/java/fr/speccy/azclientapi/bukkit/fork/popup/PopupManager.java
@@ -1,8 +1,22 @@
 package fr.speccy.azclientapi.bukkit.fork.popup;
 
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
 public class PopupManager {
 
     // Arrivera prochainement.
     // nathan818 s'y penchera plus tard.
 
+    public static void create(Integer id, String name, String[] message, ItemStack item){
+    }
+
+    public static void delete(Integer id){
+    }
+
+    public static void open(Integer id, Player player){
+    }
+
+    public static void remove(Integer id, Player player){
+    }
 }
