abeacf4722e807d14f04cc5328da3af8265d4b0e
rdxchange <136237752+rdxchange@users.noreply.github.com>
2024-04-23 15:14:57 +0200

    Update PopupManager.java

--- src/main/java/fr/speccy/azclientapi/bukkit/fork/popup/PopupManager.java
+++ src/main/java/fr/speccy/azclientapi/bukkit/fork/popup/PopupManager.java
@@ -7,23 +7,26 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PopupManager {
-    public static HashMap<Player, Integer> playerPopup = new HashMap<>();
+    public static HashMap<Integer, Player> playerPopup = new HashMap<>();
     public static HashMap<Integer, String> popup = new HashMap<>();
+    public static HashMap<Integer, String[]> popupMessage = new HashMap<>();
 
     // Arrivera prochainement.
     // nathan818 s'y penchera plus tard.
 
     public static void create(Integer id, String name, ItemStack item, String[] message){
         popup.put(id, name);
+        popupMessage.put(id, message);
     }
 
     public static void delete(Integer id){
         popup.remove(id);
+        popupMessage.remove(id);
     }
 
     public static void open(Integer id, Player player){
         if(popup.containsKey(id)){
-            playerPopup.put(player, id);
+            playerPopup.put(id, player);
         }
     }
 
